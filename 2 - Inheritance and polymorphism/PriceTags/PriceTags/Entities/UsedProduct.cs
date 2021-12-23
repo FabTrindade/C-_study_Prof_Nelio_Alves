695c0405e8f3e18dc0a5c4214c23be9afbc8d0f5
FabTrindade <fabrtrindade@gmail.com>
2021-12-23 18:12:56 -0300

    Inheritance and polymorphism exercise

---
+++ 2 - Inheritance and polymorphism/PriceTags/PriceTags/Entities/UsedProduct.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace PriceTags.Entities
+{
+    internal class UsedProduct : Product
+    {
+        public DateTime ManufactureDate { get; set; }
+
+        public UsedProduct(string name, double price, DateTime manufactureDate): base(name, price)
+        {
+            ManufactureDate = manufactureDate;
+        }
+
+        public sealed override string PriceTag()
+        {
+            return  Name + 
+                    "(used)  $" + 
+                    Price +
+                    $"(Manufacture date: {ManufactureDate.ToString("dd/MM/yyyy")})";
+        }
+    }
+}
---
+++ 2 - Inheritance and polymorphism/PriceTags/PriceTags/Program.cs
@@ -0,0 +1,59 @@
+﻿using PriceTags.Entities;
+using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace PriceTags
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of products:");
+            int n = int.Parse(Console.ReadLine());
+
+            List<Product> product = new List<Product>();
+            
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Product #{i} data:");
+                Console.Write("Common, used or imported (c/u/i)? ");
+                char c = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+                switch (c)
+                {
+                    case 'i':
+                        Console.Write("Customs fee: ");
+                        double customFee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                        product.Add(new ImportedProduct(name, price, customFee));
+                        break;
+
+                    case 'u':
+                        Console.Write("Manufacture date (DD/MM/YYYY): ");
+                        DateTime manufactureDate = DateTime.Parse(Console.ReadLine());
+                        product.Add(new UsedProduct(name, price, manufactureDate));
+                        break;
+
+
+                    case 'c':
+                        product.Add(new Product(name, price));
+                        break;
+
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("PRICE TAGS:");
+
+            foreach (Product p in product)
+            {
+                Console.WriteLine(p.PriceTag());
+            }
+        }
+    }
+}
