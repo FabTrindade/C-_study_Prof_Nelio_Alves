20c5a57d106259009385a7d244faedb98c51327f
Fabricio Trindade <fabricio.costa@sascar.com.br>
2022-01-10 21:40:11 -0300

    HashSet exercise

---
+++ 7 - Sets (Hashset and Sortedset)/NumberOfStudents/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NumberOfStudents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> CourseA = new HashSet<int>();
+            HashSet<int> CourseB = new HashSet<int>();
+            HashSet<int> CourseC = new HashSet<int>();
+
+
+            Console.Write("How many students for course A?");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                CourseA.Add(int.Parse(Console.ReadLine()));
+            }
+
+
+            Console.Write("How many students for course B?");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                CourseB.Add(int.Parse(Console.ReadLine()));
+            }
+
+
+            Console.Write("How many students for course C?");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                CourseC.Add(int.Parse(Console.ReadLine()));
+            }
+
+            HashSet<int> Total = new HashSet<int>(CourseA);
+
+            Total.UnionWith(CourseB);
+            Total.UnionWith(CourseC);
+
+            Console.Write("Total of sutudents: " + Total.Count);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 7 - Sets (Hashset and Sortedset)/Readme.md
@@ -0,0 +1,11 @@
+# Sets exercise. 
+
+
+
+#### This exercise aims to exemplify the usege of HashSet and SortedSet:
+
+
+In an online course portal, each user has a unique code, represented by an integer. Each instructor on the portal can have several courses, and the same student can enroll in as many courses as they want. Thus, the total number of students of an instructor is not simply the sum of the students of all courses that the instructor has, as there may be repeat students in more than one course. Instructor Alex has three courses A, B and C and wants to know their total number of students. His program must read students of courses A, B and C
+
+![image](https://user-images.githubusercontent.com/96549725/148860774-13f365e9-93d1-4160-b254-1682089a1a72.png)
+
