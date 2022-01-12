8b5ab84b463f23417f64b8333cec7f71cc5c8a3c
FabTrindade <fabrtrindade@gmail.com>
2022-01-11 21:29:57 -0300

    Dictionary exercise

---
+++ 8 - Dictionary/Readme.md
@@ -0,0 +1,11 @@
+# Dictionary exercise. 
+
+
+
+#### This exercise aims to exemplify the usege of Dictionary colection:
+
+
+When counting votes for an election, several voting records are generated containing the candidate's name and the number of votes (.csv format) he obtained in a ballot box. You should write a program to read the voting records from a file, and then generate a consolidated report with the totals for each candidate.
+
+![image](https://user-images.githubusercontent.com/96549725/149042174-16bcaf75-0e46-4b2f-a979-0693826268cb.png)
+
---
+++ 8 - Dictionary/VoteCount/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace VoteCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+            StreamReader sr = null;
+            Dictionary<string, int> votes = new Dictionary<string, int>();
+            try
+            {
+                using (sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] read = sr.ReadLine().Split(",");
+                        string key = read[0];
+                        int value = int.Parse(read[1]);
+
+                        if (!votes.ContainsKey(key))
+                        {
+                            votes.Add(key, value);
+                        }
+                        else
+                        {
+                            votes[key] += value;
+                        }
+                    }
+                    
+                    foreach (var v in votes)
+                    {
+                        Console.WriteLine(v.Key + ": " + v.Value);
+                    }
+                }
+
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("Error File");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Press a key to end");
+            Console.Read();
+        }
+    }
+}
