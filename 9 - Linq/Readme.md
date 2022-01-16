b3212b8db2be5267f155b6048f75beefb042a11e
FabTrindade <fabrtrindade@gmail.com>
2022-01-16 13:54:10 -0300

    Linq exercise

---
+++ 9 - Linq/Readme.md
@@ -0,0 +1,14 @@
+# Linq exercise. 
+
+
+
+#### This exercise aims to exemplify the usege of Linq library:
+
+
+Make a program to read the data (name, email and salary) of employees from a file in .csv format. Then display, in alphabetical order, the email of employees whose salary is greater than a given amount provided by the user. Also show the sum of salaries of employees whose name starts with the letter 'M'.
+
+
+
+![image](https://user-images.githubusercontent.com/96549725/149669474-c3739d91-6ab5-4add-aa04-32372e904fdf.png)
+
+![image](https://user-images.githubusercontent.com/96549725/149669484-f70ed5e4-facd-42d8-abe7-d0b72b32b7f8.png)
---
+++ 9 - Linq/SalaryConsultation/SalaryConsultation/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using SalaryConsultation.Entities;
+
+
+
+namespace SalaryConsultation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();
+
+            List <Employee> employees = new List<Employee>();   
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] lines = sr.ReadLine().Split(",");
+
+                    string name = lines[0];
+                    string email = lines[1];    
+                    double salary = double.Parse(lines[2], CultureInfo.InvariantCulture);
+
+                    employees.Add(new Employee(name, email, salary));
+                }
+
+            }
+
+            Console.Write("Enter salary: ");
+            double salaryComp = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Email of people whose salary is more than " + salaryComp.ToString("F2", CultureInfo.InvariantCulture) + ":");
+
+            var emailPrint = employees.Where(p => p.Salary > salaryComp).OrderBy(p => p.Email).Select(p => p.Email);
+
+            foreach (string mail in emailPrint)
+            {
+                Console.WriteLine(mail);
+            }
+
+            var sum = employees.Where(p => p.Name.ToUpper()[0] == 'M').Sum(p => p.Salary);
+            Console.WriteLine("Sum of salary of people whose name starts with 'M': " + sum.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
